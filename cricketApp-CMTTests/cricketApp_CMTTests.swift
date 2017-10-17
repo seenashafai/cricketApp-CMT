ca0c61925e16861bed648c8de1dbad1afb061b56
seenashafai <seenas@btinternet.com>
2017-10-17 12:50:07 +0100

    Initial Commit

---
+++ cricketApp-CMTTests/cricketApp_CMTTests.swift
@@ -0,0 +1,36 @@
+//
+//  cricketApp_CMTTests.swift
+//  cricketApp-CMTTests
+//
+//  Created by Seena Shafai on 17/10/2017.
+//  Copyright © 2017 Seena Shafai. All rights reserved.
+//
+
+import XCTest
+@testable import cricketApp_CMT
+
+class cricketApp_CMTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
