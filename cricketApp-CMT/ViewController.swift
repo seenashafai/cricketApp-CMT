d2440ec959e9f389997910bbfa110b26cdba720e
seenashafai <seenas@btinternet.com>
2017-10-17 23:32:22 +0100

    Added working server-side authentication

--- cricketApp-CMT/ViewController.swift
+++ cricketApp-CMT/ViewController.swift
@@ -9,13 +9,63 @@
 import UIKit
 import Firebase
 import FirebaseDatabase
+import FirebaseAuth
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
+
     @IBOutlet weak var inputEmail: UITextField!
-    
     @IBOutlet weak var inputPassword: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
     
+    @IBAction func loginNow(_ sender: Any)
+    {
+        if inputEmail.text != "" || inputPassword.text != ""
+        {
+            Auth.auth().signIn(withEmail: inputEmail.text!, password: inputPassword.text!) { (user, error) in
+                if user != nil
+                {
+                    // Sign in Successful
+                    print("SUCCESS")
+                    self.performSegue(withIdentifier: "segue", sender: self)
+                }
+                else
+                {
+                    if let myError = error?.localizedDescription
+                    {
+                        print(myError)
+                    }
+                    else
+                    {
+                        print("ERROR")
+                    }
+                }
+            }
+        }
+            else
+            {
+                Auth.auth().createUser(withEmail: self.inputEmail.text!, password: self.inputPassword.text!) { (user, error) in
+                    if user != nil
+                    {
+                        // Sign in Successful
+                        self.performSegue(withIdentifier: "segue", sender: self)
+
+                    }
+                    else
+                    {
+                        if let myError = error?.localizedDescription
+                        {
+                            print(myError)
+                        }
+                        else
+                        {
+                            print("ERROR")
+                        }
+                    }
+                }
+            }
+        }
     var ref: DatabaseReference!
     
     override func viewDidLoad() {
