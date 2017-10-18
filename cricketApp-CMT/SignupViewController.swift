ae0d61afbd4dc2ec744ff7db73a63bddfaeb04ec
seenashafai <seenas@btinternet.com>
2017-10-18 14:20:29 +0100

    Online push

--- cricketApp-CMT/SignupViewController.swift
+++ cricketApp-CMT/SignupViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  SignupViewController.swift
 //  cricketApp-CMT
 //
-//  Created by Seena Shafai on 17/10/2017.
+//  Created by Seena Shafai on 18/10/2017.
 //  Copyright © 2017 Seena Shafai. All rights reserved.
 //
 
@@ -11,24 +11,35 @@ import Firebase
 import FirebaseDatabase
 import FirebaseAuth
 
-class ViewController: UIViewController, UITextFieldDelegate {
-    
-    var ref: DatabaseReference!
+
+
+
+class SignupViewController: UIViewController {
     
     @IBOutlet weak var inputEmail: UITextField!
     @IBOutlet weak var inputPassword: UITextField!
-    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var confirmPassword: UITextField!
     
-    @IBAction func loginNow(_ sender: Any)
+    @IBAction func signupNow(_ sender: UIButton)
     {
+        var passMatch = true
+        if self.inputPassword != self.confirmPassword
+        {
+            // Passwords do not match
+            print("Passwords do not match")
+            passMatch = false
+            
+        }
         if inputEmail.text != "" || inputPassword.text != ""
         {
-            Auth.auth().signIn(withEmail: inputEmail.text!, password: inputPassword.text!) { (user, error) in
+            if passMatch == true
+            {
+            Auth.auth().createUser(withEmail: inputEmail.text!, password: inputPassword.text!) { (user, error) in
                 if user != nil
                 {
-                    // Sign in Successful
-                    print("Sign in successful")
+                    // Sign up Successful
                     self.performSegue(withIdentifier: "segue", sender: self)
+                    print("Register successful")
                 }
                 else
                 {
@@ -44,25 +55,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }
         }
     }
-   
-    
-    
+}
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        ref = Database.database().reference()
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        inputEmail.resignFirstResponder()
-        inputPassword.resignFirstResponder()
-        return true
+
+        // Do any additional setup after loading the view.
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        inputEmail.resignFirstResponder()
-        inputPassword.resignFirstResponder()
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -70,4 +70,3 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
 
 }
-
